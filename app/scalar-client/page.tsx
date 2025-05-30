636082ff050508b724305767166a357ff2b7812f
AbhishekNairOfficial <abhisheknairofficial>
2025-05-30 14:01:52 -0700

    Updated API

--- app/scalar-client/page.tsx
+++ app/scalar-client/page.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import { ApiReferenceReact } from '@scalar/api-reference-react'
+import orderApiSwagger from './order-api-swagger.json'
 
 import '@scalar/api-reference-react/style.css'
 import './scalar-overrides-2.css'
@@ -9,7 +10,7 @@ export default function References() {
   return (
     <ApiReferenceReact
       configuration={{
-        url: 'https://petstore.swagger.io/v2/swagger.json',
+        content: orderApiSwagger,
       }}
     />
   )
